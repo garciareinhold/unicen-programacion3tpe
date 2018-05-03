0e70e6f3e6ad0f1789b3dd23b820b66e41fddb83
german <germanwibaux@gmail.com>
2018-05-03 20:33:12 -0300

    Identacion y comentario del codigo

--- src/EntregaPrimeraParte/Test.java
+++ src/EntregaPrimeraParte/Test.java
@@ -3,16 +3,19 @@ package EntregaPrimeraParte;
 public class Test {
 
 	public static void main(String[] args) {
-	
+		
+		// Se genera la coleccion general con todos los libros
 		Herramienta tool = new Herramienta();
 		tool.setColeccion("C:/datasets/dataset4.csv");
 
+		// Se inicia el timer (para la medicion) y se genera el indice
 		Timer s = new Timer();
 		s.start();
 		int comparaciones = tool.generarIndice();		
- 
-		System.out.println("Time ->" + s.stop());
-		System.out.println("Comparations ->" + comparaciones);
+		
+		// Se muestran los resultados por pantalla
+		System.out.println("Tiempo ->" + s.stop());
+		System.out.println("Comparaciones ->" + comparaciones);
 		tool.mostrarIndice();
 
 	}
